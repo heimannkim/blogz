916ff5e2d97b2905c84e616a1481c5ecccce3373
Kim Heimann <heimannk@charter.net>
2019-05-12 20:14:27 -0500

    Added changes per Kingsley

--- main.py
+++ main.py
@@ -23,18 +23,20 @@ class Blog(db.Model):
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
-    def __init__(self, email, password):
-        self.email = email
+    def __init__(self, username, password):
+        self.username = username
         self.password = password
 
 @app.before_request
 def require_login():
+
     allowed_routes = ['login', 'index', 'list_blogs', 'signup']
-    if request.endpoint not in allowed_routes and 'email' not in session:
+    
+    if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
 @app.route('/', methods=['POST', 'GET'])
@@ -48,12 +50,15 @@ def list_blogs():
 
     oneblog = request.args.get('id')
     oneuser = request.args.get('user')
+    
     if oneblog:
         blogindi = Blog.query.get(oneblog)
         return render_template("blogindi.html", newpost=blogindi)
+    
     elif oneuser:
         user = User.query.get(oneuser)
         return render_template("singleuser.html", title="Blogs by User", blogs=user.blogs)
+    
     else:
         blogs = Blog.query.all()
         return render_template("bloglist.html", title="Build a Blog", blogs=blogs)
@@ -61,67 +66,85 @@ def list_blogs():
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
     
-    owner = User.query.filter_by(email=session['email']).first()
+    owner_id = User.query.filter_by(username=session['username']).first()
+    
     if request.method == 'POST':
         newtitle = request.form['blogtitle']
         newbody = request.form['blogbody']
+    
         if (not newtitle) or (newtitle.strip() == ''):
             flash('Please fill in the title', 'error')
+    
         if (not newbody) or (newbody.strip() == '') :
-            flash('Please fill in the body'), 'error'
+            flash('Please fill in the body', 'error')
+    
         if newtitle and newbody:
-            newpost = Blog(newtitle, newbody, owner)
+            newpost = Blog(newtitle, newbody, owner_id)
             db.session.add(newpost)
             db.session.commit()
             return render_template("blogindi.html", newpost = newpost)
+    
     return render_template("blogadd.html", title="Add a Blog Entry")
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
 
+    if 'username' in session:
+        flash('User already logged in', 'error')
+        return redirect('/')
+
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
+
         if user and user.password == password:
-            session['email'] = email
-            flash('Logged in')
+            session['username'] = username
+            flash('Logged in', 'error')
             return redirect('/newpost')
+
         elif user and user.password != password:
             flash('User password incorrect', 'error')
-            return render_template('login.html', title="Login", email=email)
+            return render_template('login.html', title="Login", username=username)
+
         else:
             flash('User does not exist', 'error')
-            return render_template('login.html', title="Login", email=email)
+            return render_template('login.html', title="Login", username=username)
+
     return render_template('login.html', title="Login")
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
 
+    if 'username' in session:
+        flash('User already logged in', 'error')
+        return redirect('/')
+
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
 
-        if email == '':
-            flash("Email can't be left blank", "error")
-            email = ''
-        elif ' ' in email:
-            flash("Email can't contain spaces", "error")        
-            email = ''
-        elif (len(email) < 3 or len(email) > 50):
-            flash('Email must be between 3 and 50 characters long', 'error')        
-            email = ''
-        elif email.count('@') != 1 and email.count('.') != 1:
-            flash('Email must contain one @ and one .(dot)', 'error')        
-            email = ''
+        if username == '':
+            flash("Username can't be left blank", "error")
+            username = ''
+
+        elif ' ' in username:
+            flash("Username can't contain spaces", "error")        
+            username = ''
+
+        elif (len(username) < 3 or len(username) > 50):
+            flash('Username must be between 3 and 50 characters long', 'error')        
+            username = ''
         
         if password == '':
             flash("Password can't be left blank", "error")
             password = ''
+
         elif ' ' in password:
             flash("Password can't contain spaces", "error")        
             password = ''
+
         elif len(password) < 3 or len(password) > 25:
             flash('Password must be between 3 and 25 characters long', 'error')        
             password = ''
@@ -129,27 +152,32 @@ def signup():
         if verify == '':
             flash("Verify can't be left blank", "error")
             verify = ''
+
         elif ' ' in verify:
             flash("Verify can't contain spaces", "error")        
             verify = ''
+
         elif len(verify) < 3 or len(verify) > 25:
             flash('Verify must be between 3 and 25 characters long', 'error')        
             verify = ''    
+
         elif verify != password:
             flash("Verify doesn't match password", "error")
             verify = ''
 
-        if not email or not password or not verify:
-            # return redirect('/signup')
-            return render_template('signup.html', title="Signup", email=email)
+        if not username or not password or not verify:
+            return render_template('signup.html', title="Signup", username=username)
+
         else:
-            existing_user = User.query.filter_by(email=email).first()
+            existing_user = User.query.filter_by(username=username).first()
+
             if not existing_user:
-                new_user = User(email,password)
+                new_user = User(username,password)
                 db.session.add(new_user)
                 db.session.commit()
-                session['email'] = email
+                session['username'] = username
                 return redirect('/newpost')
+
             else:
                 flash('User already exists', 'error')
                 
@@ -158,8 +186,8 @@ def signup():
 @app.route('/logout')
 def logout():
 
-    del session['email']
-    flash('Logged out')
+    del session['username']
+    flash('Logged out', 'error')
     return redirect('/blog')
 
 if __name__ == '__main__':
